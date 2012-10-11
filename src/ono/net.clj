753b8ba0a919f34766997d353fa66ca75be3b02d
Leo Franchi <lfranchi@kde.org>
2012-10-11 12:38:31 -0400

    Rework frame handling to use raw bytes. Uncompress zlib-compressed payloads

--- src/ono/net.clj
+++ src/ono/net.clj
@@ -1,11 +1,14 @@
 (ns ono.net
     (:require [ono.db :as db]
+              [ono.utils :as utils]
               [cheshire.core :as json]
               [gloss.io :as gio]
               [gloss.core :as gloss]
               [aleph.tcp :as tcp]
-              [lamina.core :as lamina])
-    (:import (java.net InetAddress DatagramPacket DatagramSocket)))
+              [lamina.core :as lamina]
+              [clojure.data.codec.base64 :as b64])
+    (:import (java.net InetAddress DatagramPacket DatagramSocket)
+             (java.util Arrays)))
 
 ;; Namespace for network related operations
 ;;
@@ -52,12 +55,16 @@
 (def running true)
 
 ;; Network protocol
+; (def frame (gloss/finite-frame
+;  (gloss/prefix 
+;     :int32
+;     inc
+;     dec)
+;  [:ubyte (gloss/string :utf-8)]))
+
 (def frame (gloss/finite-frame
- (gloss/prefix 
-    :int32
-    inc
-    dec)
- [:ubyte (gloss/string :utf-8)]))
+  (gloss/prefix :int32 inc dec)
+  [:ubyte (gloss/repeated :byte :prefix :none)]))
 
 ;; Utility functions
 (defn agent-error-handler
@@ -73,7 +80,7 @@
 (defn generate-json
   "Generates a vector to be serialized from a map"
   [msg-data]
-  [(flag-value :JSON) (json/generate-string msg-data)])
+  [(flag-value :JSON) (.getBytes (json/generate-string msg-data) (java.nio.charset.Charset/forName "utf-8"))])
 
 (defn get-handshake-msg
   "Returns a JSON handshake msg from zeroconf peers"
@@ -122,15 +129,27 @@
                                        port (dosync ((known-peers peer) :port))]
                                     (add-peer-connection host port peer key dbsync-connections))))))
 
+(defn uncompress
+  "Uncompresses the tcp request that has been compressed with zlib plus 4-byte big-endian size header"
+   [bytes]
+   (let [shorter (Arrays/copyOfRange bytes 4 (- (count bytes) 4))
+         uncomp  (utils/inflate shorter)
+         strval  (String. (byte-array uncomp))]
+     strval))
+
 (defn get-tcp-handler
   "Handles the TCP message for a specific peer"
   [ch peer]
-  (fn [[flag body]]
-    (println "Connection msg:" peer flag body)
-    (condp test-flag flag
-      (flag-value :SETUP) :>> (fn [_] (handle-handshake-msg ch peer flag body))
-      (flag-value :PING)  :>> (fn [_] (print))  ;; Ignore PING messages for now, TODO if no ping in 10s, disconnect
-      (flag-value :JSON)  :>> (fn [_] (handle-json-msg ch peer flag body)))))
+  (fn handle-tcp-request[[flag body-bytes]]
+    (println "Connection msg:" peer flag)
+    (if (test-flag flag (flag-value :COMPRESSED))
+      (handle-tcp-request [(bit-and (bit-not (flag-value :COMPRESSED)) flag) ;; Remove COMPRESSED flag
+                           (uncompress body-bytes)]) ;; call ourselves w/ uncompressed body
+      (let [body-str (java.lang.String. (byte-array body-bytes) (java.nio.charset.Charset/forName "utf-8"))]
+        (condp test-flag flag
+          (flag-value :SETUP) :>> (fn [_] (handle-handshake-msg ch peer flag body-str))
+          (flag-value :PING)  :>> (fn [_] (print))  ;; Ignore PING messages for now, TODO if no ping in 10s, disconnect
+          (flag-value :JSON)  :>> (fn [_] (handle-json-msg ch peer flag body-str)))))))
 
 (defn add-peer-connection
     "Adds a new peer's connection (main ControlConnection or secondary connection)
