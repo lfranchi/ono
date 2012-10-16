799c4314db771a0e89eac3e42003539b9d862693
Zachary Allaun <zach.allaun@gmail.com>
2012-10-16 15:46:39 -0400

    Make test work on different computers.

--- test/ono/test/core.clj
+++ test/ono/test/core.clj
@@ -1,7 +1,7 @@
 (ns ono.test.core
   (:use [ono.core]
         [midje.sweet])
-   (:require [fs.core :as fs]))
+  (:require [fs.core :as fs]))
 
 (fact (filetype-supported? ".mp3")  => truthy)
 (fact (filetype-supported? ".ogg")  => truthy)
@@ -11,15 +11,22 @@
 (fact (filetype-supported? ""    )  => falsey)
 
 (defn mute-stderr
-	"Quiet stderr when calling the desired functions"
-	[f]
-	(fn [& args]
-		(let [old-err (System/err)
-			  _       (System/setErr (new java.io.PrintStream (new java.io.FileOutputStream "/dev/null")))
-		      retval (apply f args)]
-			(System/setErr old-err)
-			retval)))
+  "Quiet stderr when calling the desired functions"
+  [f]
+  (fn [& args]
+    (let [old-err (System/err)
+          _       (System/setErr (new java.io.PrintStream (new java.io.FileOutputStream "/dev/null")))
+          retval (apply f args)]
+      (System/setErr old-err)
+      retval)))
 
-(fact 
-	(let [testfile (fs/file "resources/test_data/test_file.mp3")]
-		((mute-stderr #'ono.core/extractID3) testfile) => {:duration 280, :artist "Marc Reeves", :year "", :size 7127061, :bitrate 44100, :album "Perfectly Fine (remastered)", :url "/Users/leo/src/ono/resources/test_data/test_file.mp3", :mtime 1350414053000, :albumpos "1", :track "Handshakes", :source nil}))
+(defn keyvals= [baseline-map]
+  (fn [m]
+    (every? #(= (% m) (% baseline-map)) (keys baseline-map))))
+
+(fact
+ (let [testfile (fs/file "resources/test_data/test_file.mp3")]
+   ((mute-stderr #'ono.core/extractID3) testfile) => (keyvals= {:duration 280, :artist "Marc Reeves", :year "", :size 7127061, :bitrate 44100, :album "Perfectly Fine (remastered)", :albumpos "1", :track "Handshakes", :source nil})))
+
+
+
