2373a3bfacd4ce4138dfbd7a821c59f2f460cfbf
Leo Franchi <lfranchi@kde.org>
2013-03-18 22:22:29 -0400

    Update project.clj for lein2

--- project.clj
+++ project.clj
@@ -2,20 +2,21 @@
   :description "A tomahawk daemon written in Clojure"
   :license {:name "GNU Public License V3"
             :url "http://www.gnu.org/licenses/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-        				 [fs "1.3.2"] ;; Filesystem/shell tools
-        				 [cheshire "4.0.3"] ;; JSON lib
-        				 [korma "0.3.0-beta7"] ;; SQL wrapper
-        				 [org.clojure/java.jdbc "0.1.1"] 
-        				 [org.xerial/sqlite-jdbc "3.7.2"]
-        				 [org.jaudiotagger/jaudiotagger "2.0.1"] ;; ID3/FLAC tagging library
-                 [aleph "0.3.0-beta2"] ;; TCP abstraction layer
-                 [log4j/log4j "1.2.16"]] ;; Logging
-   :dev-dependencies [[lein-midje "1.0.10"]
-                      [midje "1.4.0"] ;; unit testing 
-                      [com.stuartsierra/lazytest "1.2.3"]
-                      [org.clojure/data.codec "0.1.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [fs "1.3.3"] ;; Filesystem/shell tools
+                 [cheshire "5.0.2"] ;; JSON lib
+                 [korma "0.3.0-RC4"] ;; SQL wrapper
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]
+                 [org.jaudiotagger/jaudiotagger "2.0.1"] ;; ID3/FLAC tagging library
+                 [aleph "0.3.0-beta14"] ;; TCP abstraction layer
+                 [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jdmk/jmxtools
+                                                    com.sun.jmx/jmxri]]] ;; Logging
 
-  :repositories {"stuart" "http://stuartsierra.com/maven2"}
-
+  :profiles {:dev {:dependencies [[midje "1.5.0"] ;; unit testing
+                                  [org.clojure/data.codec "0.1.0"]]
+                   :plugins [[lein-midje "3.0.0"]]}
+             :user {:plugins [[lein-kibit "0.0.8"]]}}
   :main ono.core)
